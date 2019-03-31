5b15a89096596df8051197516e2d1a89f5e50e68
Cesar Carlos <CCarlos@walmartlabs.com>
2019-03-30 19:32:06 -0700

    include schema csv parser

--- challenge.py
+++ challenge.py
@@ -1,5 +1,44 @@
 import argparse
+import csv
 
+
+class UnknownDataTypeException(Exception):
+    pass
+
+
+class DataLoader:
+    def __init__(self):
+        self.data_type_mapping = {
+            'INT': 'INTEGER',
+            'INTEGER': 'INTEGER',
+
+            'CHAR': 'TEXT',
+            'VARCHAR': 'TEXT',
+            'TEXT': 'TEXT',
+
+            'BOOLEAN': 'NUMERIC'
+        }
+
+    def _create_table(self, table_name='schema', schema=''):
+        table_create_statement = '''CREATE TABLE IF NOT EXISTS %s (%s)'''
+
+    def _process_schema(self, filename='schema.csv'):
+        # read in csv file and match it against the data_type_mapping
+        data_list = []
+        data_type_keys = self.data_type_mapping.keys()
+        with open(filename, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                if row['datatype'].upper() not in data_type_keys:
+                    raise UnknownDataTypeException(
+                        "Data type not supported: %s" % row['datatype'])
+                data_list.append((row['field name'], row['width'], row['datatype']))
+
+        return data_list
+
+    def _schema_parser(self):
+        """Create a mapping of possible data types => sqlite"""
+        pass
 
 def store():
     pass
@@ -17,5 +56,10 @@ def main():
     args = parser.parse_args()
     print(args.tablename)
     print(args.droptable)
+    try:
+        dl = DataLoader()
+        dl._process_schema('schema.csv')
+    except UnknownDataTypeException as e:
+        print("An exception occurred: %s" % e)
 
 main()
